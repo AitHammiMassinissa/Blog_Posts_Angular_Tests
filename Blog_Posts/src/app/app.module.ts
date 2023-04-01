3d982c97dc509f65bf2e3521688b396e6c0c914c
Massinissa Ait Hammi <massinissaait2018@gmail.com>
2023-04-01 03:03:12 +0200

    Creation of "blog" and "blog-post" components with the ng generate component command

--- Blog_Posts/src/app/app.module.ts
+++ Blog_Posts/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    BlogPostComponent
   ],
   imports: [
     BrowserModule,
