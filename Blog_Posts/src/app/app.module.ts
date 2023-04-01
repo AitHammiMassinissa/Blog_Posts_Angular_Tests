0e9c9b079bbe08d6940a61c768bb7c71d9aa94bf
Massinissa Ait Hammi <massinissaait2018@gmail.com>
2023-04-02 01:21:26 +0200

    add a header and import the different components needed in app.module.ts

--- Blog_Posts/src/app/app.module.ts
+++ Blog_Posts/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './blog/blog.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
+import { StripHtmlPipe } from './strip-html.pipe';
+import { BlogService } from './blog.service';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlogComponent,
-    BlogPostComponent
+    BlogPostComponent,
+    StripHtmlPipe,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BlogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
